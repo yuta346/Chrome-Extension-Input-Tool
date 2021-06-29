465ebf5b348e92f16fb21cd23e5489d654fc0e05
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-06-29 14:34:48 +0900

    add comment to popup and content.js

--- content.js
+++ content.js
@@ -2,5 +2,5 @@
 chrome.runtime.onMessage.addListener(getMessage);
         
 function getMessage(message, sender, sendResponse) {
-    console.log(message.message);  
+    console.log(message.message);  //display the message from popup window for debug
 }
--- popup.js
+++ popup.js
@@ -13,13 +13,12 @@ function setup(){
                                   headers: {"Content-Type": "application/json"},
                                   body: JSON.stringify({word:userInput})}  
 
-                  const response = await fetch("http://127.0.0.1:5000/", config)
+                  const response = await fetch("http://127.0.0.1:5000/", config);
                   
-                  const data = await response.json()
+                  const data = await response.json();
 
-                  chrome.tabs.sendMessage(tabs[0].id, {message: data.word})
+                  chrome.tabs.sendMessage(tabs[0].id, {message: data.word});  //send message to content.js to displau in the console
                 }
                 sendToDatabase()
-
               }) 
 }
