66f36cbc665425989ae95c6e3e17d635f061992e
Yuta Hagiwara <yhagiwara@outlook.com>
2021-06-29 14:28:57 +0900

    add api call

--- popup.js
+++ popup.js
@@ -5,8 +5,22 @@ function setup(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 let input = document.getElementById("word");
                 let userInput = input.value;
-                console.log(userInput)
-                console.log(tabs)
-                chrome.tabs.sendMessage(tabs[0].id, {message: userInput})
+
+                async function sendToDatabase(){
+
+                  const config = {
+                                  method: "POST",
+                                  headers: {"Content-Type": "application/json"},
+                                  body: JSON.stringify({word:userInput})}  
+
+                  const response = await fetch("http://127.0.0.1:5000/", config)
+                  
+                  const data = await response.json()
+
+                  chrome.tabs.sendMessage(tabs[0].id, {message: data.word})
+                }
+                sendToDatabase()
+
+                
               }) 
 }
