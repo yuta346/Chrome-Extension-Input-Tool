ebb414ba557f4e47be399ce7ca51bafece32939c
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-06-29 17:24:01 +0900

    add status message dynamically to the popup window using appendChild

--- popup.js
+++ popup.js
@@ -3,6 +3,12 @@ document.querySelector("button").addEventListener("click", setup);
 
 function setup(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+                let existingMessage = document.getElementById("statusMessage");
+                if(existingMessage){
+                  existingMessage.remove()
+                }
+
                 let input = document.getElementById("word");
                 let userInput = input.value;
 
@@ -16,6 +22,21 @@ function setup(){
                   const response = await fetch("http://127.0.0.1:5000/", config);
                   
                   const data = await response.json();
+
+                  let newParagraph = document.createElement('p');
+                  newParagraph.setAttribute("id", "statusMessage");
+
+                  let cardBody = document.getElementsByClassName("card-body")[0];
+                  
+                  if(data.status === "success"){
+                    var text = document.createTextNode("Added Successfully ")
+                    newParagraph.appendChild(text);
+                    cardBody.appendChild(newParagraph);
+                  }else{
+                    var text = document.createTextNode("Error. This word is not present in the dictionary.")
+                    newParagraph.appendChild(text);
+                    cardBody.appendChild(newParagraph);
+                  }
                   
                   chrome.tabs.sendMessage(tabs[0].id, {result:data.status, word:data.word});  //send message to content.js to display in the console
                 }
