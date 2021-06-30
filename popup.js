ace2fe60d9b1b2b2306080b58dca040540e05945
Yuta Hagiwara <hagiwara.y@outlook.com>
2021-06-30 15:02:35 +0900

    update url in async function

--- popup.js
+++ popup.js
@@ -19,7 +19,7 @@ function setup(){
                                   headers: {"Content-Type": "application/json"},
                                   body: JSON.stringify({word:userInput})}  
 
-                  const response = await fetch("http://127.0.0.1:5000/", config);
+                  const response = await fetch("http://127.0.0.1:5000/add/popup", config);
                   
                   const data = await response.json();
 
